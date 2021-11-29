38f8e95e7de130dc4966a4f6444743b48f72cd59
Calvin Remsburg <cremsburg@protonmail.com>
2021-11-29 11:21:48 +0000

    remove create url

--- apbr/urls.py
+++ apbr/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create/', views.RandomApbrProfileView.as_view(), name='random_profile'),
     path('random/', views.RandomApbrProfileView.as_view(), name='random_profile'),
 ]
